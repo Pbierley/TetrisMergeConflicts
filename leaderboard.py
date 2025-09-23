4b6334ec97f3cb507a934f811c9f7a862df3b484
Jolteer <50204109+Jolteer@users.noreply.github.com>
2025-09-23 13:06:09 -0400

    Made leaderboard than can pull data from json

---
+++ leaderboard.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_json('D:\Programming And Cyber Security\School\Software Engineer\ASE 420\Group Project\TetrisMergeConflicts\leaderboard.json')
+
+print(df.head(10))
